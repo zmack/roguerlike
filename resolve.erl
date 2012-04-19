2ce5849668dd30eed9952f72e71cc7dd54a5dee1
Andrei Bocan <zmaxor@gmail.com>
2012-04-19 15:23:51 +0200

    Resolve tile

--- resolve.erl
+++ resolve.erl
@@ -2,9 +2,18 @@
 -include_lib("eunit/include/eunit.hrl").
 
 -include("creatures.hrl").
+-import(lists).
 
 -export([resolve/2, get_mob_damage/1]).
 
+is_tile_resolved(#tile{ creatures = [] }) ->
+  true;
+
+is_tile_resolved(#tile{ creatures = Creatures }) ->
+  lists:all(fun (A) -> is_creature_dead(A) end, Creatures).
+
+is_creature_dead(#creature{ health = Health }) ->
+  Health == 0.
 
 resolve(Player, Tile) when is_record(Tile, tile) ->
   #tile{ creatures = Creatures } = Tile,
@@ -61,4 +70,22 @@ resolve_player_doesnt_die_test() ->
   { NewPlayer, _NewTile } = resolve(Player, Tile),
 
   ?assertEqual(18, NewPlayer#creature.health).
+
+is_tile_resolved_works_for_empty_lists_test() ->
+  Tile = #tile { creatures = [ ] },
+  ?assertEqual(true, is_tile_resolved(Tile)).
+
+is_tile_resolved_works_for_lists_test() ->
+  FirstCreature = #creature { health = 0, damage = 1 },
+  SecondCreature = #creature { health = 0, damage = 1 },
+  Tile = #tile { creatures = [ FirstCreature, SecondCreature ] },
+
+  ?assertEqual(true, is_tile_resolved(Tile)).
+
+is_tile_resolved_returns_false_for_nondead_creature_lists_test() ->
+  FirstCreature = #creature { health = 0, damage = 1 },
+  SecondCreature = #creature { health = 1, damage = 1 },
+  Tile = #tile { creatures = [ FirstCreature, SecondCreature ] },
+
+  ?assertEqual(false, is_tile_resolved(Tile)).
 
