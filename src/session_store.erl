a6b23aec4008b8ac9dd8f9b1a59b6408f4d98391
Alin Popa <alin.popa@gmail.com>
2012-05-04 08:06:09 -0400

    Simplify the session_store api; unit tests.

--- src/session_store.erl
+++ src/session_store.erl
@@ -3,12 +3,15 @@
 -export([
          init/0,
          insert/1,
-         delete_pid/1,
-         delete_key/1,
+         delete/1,
          lookup/1
         ]).
 
 -define(TABLE_ID, ?MODULE).
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
 
 init() ->
   ets:new(?TABLE_ID, [public, named_table]),
@@ -24,12 +27,77 @@ lookup(Key) ->
     []           -> {error, not_found}
   end.
 
-delete_pid(Pid) ->
-  ets:match_delete(?TABLE_ID, {'_', Pid}).
-
-delete_key(Key) ->
-  ets:match_delete(?TABLE_ID, {Key, '_'}).
+delete({pid, Pid}) ->
+  ets:match_delete(?TABLE_ID, {'_', Pid});
+delete({key, Key}) ->
+  ets:match_delete(?TABLE_ID, {Key, '_'});
+delete(_Any) -> true.
 
 generate_key() ->
   { MegaSeconds, Seconds, Microseconds } = now(),
   base64:encode(<<MegaSeconds:32,Seconds:32, Microseconds:32>>).
+
+%% =========================================================
+%% EUnit tests
+%% =========================================================
+-ifdef(TEST).
+
+init_should_not_fail_test() ->
+  InitResponse = init(),
+
+  ?assertEqual(ok, InitResponse).
+
+insert_should_be_successfully_performed_test() ->
+  InsertResponse = insert(test_pid),
+
+  ?assertEqual(true, InsertResponse).
+
+loopup_for_nonexisting_key_should_return_not_found_test() ->
+  Value = lookup(test_key),
+
+  ?assertEqual({error, not_found}, Value).
+
+lookup_for_valid_key_should_return_valid_value_test() ->
+  ets:insert(?TABLE_ID, {test_key, some_value_here}),
+
+  Value = lookup(test_key),
+
+  ?assertEqual({ok, some_value_here}, Value).
+
+delete_wrong_expression_should_return_true_test() ->
+  DeleteResponse = delete("Some other expression"),
+
+  ?assertEqual(true, DeleteResponse).
+
+delete_nonexisting_key_should_return_true_test() ->
+  DeleteResponse = delete({key, some_key}),
+
+  ?assertEqual(true, DeleteResponse).
+
+delete_nonexisting_pid_should_return_true_test() ->
+  DeleteResponse = delete({pid, some_pid}),
+
+  ?assertEqual(true, DeleteResponse).
+
+delete_existing_key_should_return_true_test() ->
+  ets:insert(?TABLE_ID, {some_key, some_pid}),
+
+  DeleteResponse = delete({key, some_key}),
+
+  ?assertEqual(true, DeleteResponse).
+
+delete_existing_pid_should_return_true_test() ->
+  ets:insert(?TABLE_ID, {some_key, some_pid}),
+
+  DeleteResponse = delete({pid, some_pid}),
+
+  ?assertEqual(true, DeleteResponse).
+
+delete_nonexisting_key_should_not_affect_existing_entries_test() ->
+  ets:insert(?TABLE_ID, {some_key, some_pid}),
+
+  delete({key, some_wrong_key}),
+
+  ?assertEqual([{some_key, some_pid}], ets:lookup(?TABLE_ID, some_key)).
+
+-endif.
