d6bb1465707828707344529255bd1befe56759ed
Alin Popa <alin.popa@gmail.com>
2012-05-10 11:24:26 +0300

    Session -> Instance

--- src/roguerlike_sup.erl
+++ src/roguerlike_sup.erl
@@ -21,7 +21,7 @@ start_link() ->
   supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 start_child(Player, Dungeon) ->
-  Key = session_store:generate_key(),
+  Key = instance_store:generate_key(),
   case supervisor:start_child(?SERVER, [Player, Dungeon, Key]) of
     { ok, _Pid } -> {ok, Key};
     Response -> Response
